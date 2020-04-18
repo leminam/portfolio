5a7f6848377dafac35d1954ef523004f55622061
Maria Lemina <lemina.m@gmail.com>
2020-04-18 21:33:43 +0300

    viewer content updates

--- src/static/js/init/data.js
+++ src/static/js/init/data.js
@@ -7,55 +7,81 @@ document.addEventListener("DOMContentLoaded", function() {
         title: "Vivamus pulvinar",
         info_description: "Vivamus pulvinar sem non urna vulputate, a molestie nibh volutp. Ut at sem nec justo consequat",
         devices_description: "Vivamus pulvinar sem non urna vulputate, a molestie nibh volutp. Ut at sem nec justo consequat",        
-        screenshot: "static/img/john1-desk.png",
+        screenshot: "static/img/content/musician/john1-desk.png",
         images: [
           {
             type: 'desktop',
             items: [
-              {src :'static/img/john1-desk.png', type: 'desktop'},
-              {src: 'static/img/john2-desk.png', type: 'desktop'},
-              {src: 'static/img/john3-desk.png', type: 'desktop'},
+              {src :'static/img/content/musician/john1-desk.png', type: 'desktop'},
+              {src: 'static/img/content/musician/john2-desk.png', type: 'desktop'},
+              {src: 'static/img/content/musician/john3-desk.png', type: 'desktop'},
             ]
           },
           {
             type: 'tablet',
             items: [
-              {src :'static/img/john4-tab.png', type: 'tablet'},
-              {src: 'static/img/john5-tab.png', type: 'tablet'},
-              {src: 'static/img/john6-tab.png', type: 'tablet'},
+              {src :'static/img/content/musician/john4-tab.png', type: 'tablet'},
+              {src: 'static/img/content/musician/john5-tab.png', type: 'tablet'},
+              {src: 'static/img/content/musician/john6-tab.png', type: 'tablet'},
             ]
           },
           {
             type: 'mobile',
             items: [
-              {src :'static/img/john7-mob.png', type: 'mobile'},
-              {src: 'static/img/john8-mob.png', type: 'mobile'},
-              {src: 'static/img/john9-mob.png', type: 'mobile'},
+              {src :'static/img/content/musician/john7-mob.png', type: 'mobile'},
+              {src: 'static/img/content/musician/john8-mob.png', type: 'mobile'},
+              {src: 'static/img/content/musician/john9-mob.png', type: 'mobile'},
             ]
           },
         ]
       },
       { logo: "static/img/icons/alibi.png",
-        previewBackgroundClass: "portfolio__preview-background_tab",
-        portfolioPreviewDevice: "portfolio__preview-device_tab",
-        portfolioPreviewImage: "portfolio__preview-image_tab",
-        device: "tablet",        
-        device_wrapper: "static/img/mac_1.png",
         title: "Vivamus pulvinar",
         info_description: "Vivamus pulvinar sem non urna vulputate, a molestie nibh volutp. Ut at sem nec justo consequat",
         devices_description: "Vivamus pulvinar sem non urna vulputate, a molestie nibh volutp. Ut at sem nec justo consequat",        
-        screenshot: "static/img/john2-desk.png",
+        screenshot: "static/img/content/tamtam/tam5-desk.png",
+        images: [
+          {
+            type: 'desktop',
+            items: [
+              {src :'static/img/content/tamtam/tam1-desk.png', type: 'desktop'},
+              {src: 'static/img/content/tamtam/tam5-desk.png', type: 'desktop'},
+              {src: 'static/img/content/tamtam/tam6-desk.png', type: 'desktop'},
+            ]
+          },
+          {
+            type: 'tablet',
+            items: [
+              {src :'static/img/content/tamtam/tam2-tab.png', type: 'tablet'},
+              {src: 'static/img/content/tamtam/tam3-tab.png', type: 'tablet'},
+              {src: 'static/img/content/tamtam/tam4-tab.png', type: 'tablet'},
+            ]
+          },
+          {
+            type: 'mobile',
+            items: [
+              {src :'static/img/content/tamtam/tam7-mob.png', type: 'mobile'},
+              {src: 'static/img/content/tamtam/tam8-mob.png', type: 'mobile'},
+              {src: 'static/img/content/tamtam/tam9-mob.png', type: 'mobile'},
+            ]
+          },
+        ]        
       },
       { logo: "static/img/icons/ave.png",
-        previewBackgroundClass: "portfolio__preview-background_mob",
-        portfolioPreviewDevice: "portfolio__preview-device_mob",
-        portfolioPreviewImage: "portfolio__preview-image_mob",
-        device: "mobile",        
-        device_wrapper: "static/img/mac_1.png",
         title: "Vivamus pulvinar",
         info_description: "Vivamus pulvinar sem non urna vulputate, a molestie nibh volutp. Ut at sem nec justo consequat",
         devices_description: "Vivamus pulvinar sem non urna vulputate, a molestie nibh volutp. Ut at sem nec justo consequat",        
-        screenshot: "static/img/john3-desk.png",
+        screenshot: "static/img/content/ave/ave1-desk.png",
+        images: [
+          {
+            type: 'desktop',
+            items: [
+              {src :'static/img/content/ave/ave1-desk.png', type: 'desktop'},
+              {src: 'static/img/content/ave/ave2-desk.png', type: 'desktop'},
+              {src: 'static/img/content/ave/ave3-desk.png', type: 'desktop'},
+            ]
+          },
+        ]
       },
     ]
   });
